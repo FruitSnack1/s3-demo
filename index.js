c0bca0b611782ab39ddc5275dcf307732df41092
snapchathotdog <tomas.ryzner@email.cz>
2021-05-29 17:50:44 +0200

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+import AWS from 'aws-sdk'
+import fs from 'fs'
+
+// Enter copied or downloaded access ID and secret key here
+const ID = '';
+const SECRET = '';
+
+// The name of the bucket that you have created
+const BUCKET_NAME = 'poopeek-test';
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const params = {
+    Bucket: BUCKET_NAME,
+    CreateBucketConfiguration: {
+        // Set your region here
+        LocationConstraint: "eu-central-1"
+    }
+};
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: 'vvv.png', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function (err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
+
+uploadFile('vvv.png')
